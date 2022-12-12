f174627241ab4fdd5d61f5c08c17d72a7336aa74
anna <anna@sayf.su>
2022-12-12 15:03:10 +0300

    доработана задача 9

--- src/Main.java
+++ src/Main.java
@@ -72,8 +72,8 @@ public class Main {
         int deposit = 29000;
         int total = 0;
         for (int i = 1; i <= 12; i++) {
-            total = total + total / 100;
             total = total + deposit;
+            total = total + total / 100;
             System.out.println("Месяц " + i + ", сумма накоплений равна " + total + " рублей");
         }
     }
